c782887289e516ee34f5c49f536b4c41b5248c52
shima <shima.jin@icloud.com>
2017-02-17 21:44:14 +0900

    Initial Commit

---
+++ MVVM-Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVM-Practice
+//
+//  Created by 島仁誠 on 2017/02/17.
+//  Copyright © 2017年 jinsei shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
