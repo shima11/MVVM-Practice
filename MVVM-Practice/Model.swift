98b8f29afe4884bf6590247d6ad1277077dee909
shima <shima.jin@icloud.com>
2017-02-19 00:02:05 +0900

    Add Model.swift, ViewModel.swift

---
+++ MVVM-Practice/Model.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  MVVM-Practice
+//
+//  Created by 島仁誠 on 2017/02/18.
+//  Copyright © 2017年 jinsei shima. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    
+    var text = ""
+    
+    init() {
+        
+    }
+}
---
+++ MVVM-Practice/ViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  ViewModel.swift
+//  MVVM-Practice
+//
+//  Created by 島仁誠 on 2017/02/18.
+//  Copyright © 2017年 jinsei shima. All rights reserved.
+//
+
+import Foundation
+import Bond
+
+class ViewModel {
+    
+    var text = ""
+    var model = Observable(Model())
+    
+    init() {
+        
+    }
+    
+}
